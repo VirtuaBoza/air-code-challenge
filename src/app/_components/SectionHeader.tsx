5cd8e9ee84a504515cc0019eeabfdcf33a915ba4
Andrew Boza <aboza@madmobile.com>
2024-01-17 15:22:02 -0500

    adjust headers

--- src/app/_components/SectionHeader.tsx
+++ src/app/_components/SectionHeader.tsx
@@ -1,6 +1,6 @@
 export function SectionHeader({ label }: { label: string }) {
   return (
-    <h2 className="font-semibold leading-none tracking-tight uppercase pb-8 text-primary text-sm">
+    <h2 className="font-semibold leading-none tracking-tight uppercase pb-6 text-slate-600 text-xs">
       {label}
     </h2>
   );
