555162a1b29c5f54bcff8f7214115debaeba5485
Andrew Boza <aboza@madmobile.com>
2024-01-17 15:24:47 -0500

    rm unused scroll container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import { BoardsSection } from "./_components/BoardsSection";
 import { AssetsSection } from "./_components/AssetsSection";
-import { useRef } from "react";
 
 export default function Home() {
-  const scrollContainer = useRef<HTMLElement>(null);
   return (
-    <main ref={scrollContainer} className="py-8 px-12 overflow-y-auto">
+    <main className="py-8 px-12 overflow-y-auto">
       <BoardsSection />
       <AssetsSection />
     </main>
