b42d6af4fc4c3b261ffcb66172a1957358be84b8
Andrew Boza <aboza@madmobile.com>
2024-01-17 13:47:18 -0500

    try NEXT_PUBLIC_VERCEL_URL

--- src/lib/trpc.client.tsx
+++ src/lib/trpc.client.tsx
@@ -13,7 +13,10 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${process.env.VERCEL_URL || "http://localhost:3000"}/api/trpc`,
+          url:
+            (process.env.NEXT_PUBLIC_VERCEL_URL
+              ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+              : "http://localhost:3000") + "/api/trpc",
         }),
       ],
     })
